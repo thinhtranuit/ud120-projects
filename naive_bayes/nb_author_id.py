8d41378bd0a7103f791564b12359461ad85b987c
Thinh Tran <thinhuit@gmail.com>
2019-07-09 08:28:32 +0700

    finish quiz about naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,24 +9,31 @@
     Sara has label 0
     Chris has label 1
 """
-    
-import sys
+
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
-### your code goes here ###
-
+# your code goes here ###
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time: {:.2f}".format(time()-t0))
+GaussianNB(priors=None, var_smoothing=1e-09)
+t1 = time();
+predict = clf.predict(features_test)
+print("Predicting time: {:.2f}".format(time()-t1))
+print(predict)
+score = clf.score(features_test, labels_test)
+print(score)
 
 #########################################################
 
